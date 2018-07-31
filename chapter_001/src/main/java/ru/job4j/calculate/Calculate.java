627881c6c81c5860e4a440b98852aae0ab8f97cf
pvladi7 <mailto:w.h.81@mail.ru>
2018-07-31 22:10:19 +0600

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,10 @@
 package ru.job4j.calculate;
 
 public class Calculate{
+	/** 
+	*   The main method. Output the string to the console.
+	*   @param args -args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
