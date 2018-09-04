b9eeb7117015fa0257d1fe0f149ba92226f187c1
pvladi7 <mailto:w.h.81@mail.ru>
2018-09-04 23:47:05 +0600

    6.5. Создать программу для сортировки массива методом перестановки.(Расческа)

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -1,25 +1,51 @@
 package ru.job4j.array;
 
 /**
- * class BubbleSort
+ * class BubbleSortComb
  */
 public class BubbleSort {
     /**
-     * method sort
+     * method sort algorithm comb
      * @param array
      * @return array sort
      */
+
+    private double reduction_factor = 1.247390;
+    private int num_permutations;
+
+    private void swap(int[] array, int i) {
+        int temp;
+        temp =  array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+    }
+
     public int[] sort(int[] array) {
-        int tmp;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
+        int step = array.length - 1;
+        int temp;
+        while (step >= 1) {
+            for (int i = 0; i + step < array.length; ++i) {
+                if (array[i] > array[i + step]) {
+                    swap(array, i);
+                   num_permutations++;
+                }
+            } step /= reduction_factor;
+        }
+
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
-                  tmp = array[j + 1];
-                  array[j + 1] = array[j];
-                  array[j] = tmp;
+                    swap(array, j);
+                    swapped = true;
+                    ++num_permutations;
                 }
             }
+            if (!swapped)
+                break;
         }
         return array;
     }
+
+
 }
