c953714d7e13849b75705ff2c19d7ab890bb84dc
pvladi7 <mailto:w.h.81@mail.ru>
2019-02-08 00:00:02 +0600

    6.5. Создать программу для сортировки массива методом перестановки.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -5,47 +5,30 @@ package ru.job4j.array;
  */
 public class BubbleSort {
     /**
-     * method sort algorithm comb
+     * method bubble sort
      * @param array
      * @return array sort
      */
 
-    private double reduction_factor = 1.247390;
-    private int num_permutations;
-
-    private void swap(int[] array, int i) {
-        int temp;
-        temp =  array[i];
-        array[i] = array[i+1];
-        array[i+1] = temp;
-    }
 
     public int[] sort(int[] array) {
-        int step = array.length - 1;
-        int temp;
-        while (step >= 1) {
-            for (int i = 0; i + step < array.length; ++i) {
-                if (array[i] > array[i + step]) {
-                    swap(array, i);
-                   num_permutations++;
-                }
-            } step /= reduction_factor;
-        }
-
-        for (int i = 0; i < array.length - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    swap(array, j);
-                    swapped = true;
-                    ++num_permutations;
-                }
+        int ind = 0;
+        int length = array.length - 1;
+        boolean flag;
+        for (int i = 0; i <= length; i++) {
+            flag = true;
+            if(i == length){
+                i = 0; ind = 0; length--;
             }
-            if (!swapped)
-                break;
+            if (array[ind] > array[ind + 1]) {
+                array[ind]     ^= array[ind + 1];
+                array[ind + 1] ^= array[ind];
+                array[ind]     ^= array[ind + 1];
+                flag = false;
+            }
+            if ((flag)&& (i == length)) {break;}
+            ind++;
         }
         return array;
     }
-
-
 }
